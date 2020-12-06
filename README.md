3cc7a8ab30c8d07625a5bde3d2ab2af669bda6e3
Hendrik Bulens <hendrik.bulens@gmail.com>
2020-12-06 17:07:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center"><img src="assets/spreadsheet.svg?raw=true" width="350" alt=""></p>
+
+# List to Excel
+
+Simple library to convert IEnumerable<T> to an Excel sheet.
+
+## About the project
+
+## Installation
+
+> 🚧 The packages are not available yet on NuGet.
+
+## Usage
+
+TO DO
+
+## Contributing
+
+![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)
+
+Pull requests are welcome. Please check out the contribution and code of conduct guidelines.
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
---
+++ src/core/HttpRequestMessageExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace System.Net.Http
+{
+    public static class HttpRequestMessageExtensions
+    {
+        private static object GetPropValue(this object src, string propName)
+            => src.GetType().GetProperty(propName).GetValue(src, null);
+
+        public static HttpResponseMessage ExportToExcel<T>(this HttpRequestMessage request, IEnumerable<T> records)
+        {
+            using ExcelPackage package = new ExcelPackage();
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Logs");
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            using (ExcelRange range = worksheet.Cells[1, 1, 1, properties.Length])
+            {
+                range.Style.Font.Bold = true;
+                range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                range.Style.Fill.BackgroundColor.SetColor(Color.Purple);
+                range.Style.Font.Color.SetColor(Color.White);
+            }
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                PropertyInfo property = properties.ElementAt(i);
+                worksheet.Cells[1, i + 1].Value = property.Name;
+            }
+
+            int rowNo = 2;
+            for (int i = 0; i < records.Count(); i++)
+            {
+                T record = records.ElementAt(i);
+                for (int y = 0; y < properties.Length; y++)
+                {
+                    worksheet.Cells[rowNo, y + 1].Value = record.GetPropValue(properties.ElementAt(y).Name)?.ToString() ?? "";
+                }
+
+                rowNo += 1;
+            }
+
+            worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+            using MemoryStream stream = new MemoryStream(package.GetAsByteArray());
+            ByteArrayContent content = new ByteArrayContent(stream.ToArray());
+            HttpResponseMessage result = request.CreateResponse(HttpStatusCode.OK);
+            result.Content = content;
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            result.Content.Headers.ContentDisposition.FileName = $"Export-{DateTime.Now:yyyy-MM-dd h:mm tt}.xlsx";
+
+            return result;
+        }
+    }
+}
